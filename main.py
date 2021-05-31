4c8fc49e130947875982def7857f032f2848da6b
Ana Marraco <48214868+amarraco@users.noreply.github.com>
2021-05-31 15:49:29 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+
+#planteamiento
+
+# conectar a la bbdd
+    # obtener numeros absolutos cargados para el dia "x"
+    # obtener numeros filtrado (numero de vehiculos distintos)
+    # obtener numero de veces que leemos un vehiculos --> ratio de lectura dividir el resultado count de las dos anteriores
+
+#estadisticas:
+# conectar a bbdd:
+    # obtener fecha hoy
+    #restarle 7
+    # preguntar por los accesos distinct
+    #restarle 14
+    # preguntar por los accesos distinct
+    #restarle 21
+    # preguntar por los accesos distinct
+
+#sacar media
+#comparar % accesos del día
